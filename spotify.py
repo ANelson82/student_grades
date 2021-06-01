5be759bcd72cac08caba047d2a76a5448cc2a4c2
Andrew Nelson <andycnelson@gmail.com>
2021-06-01 12:31:05 -0500

    added a comment line on #13

--- spotify.py
+++ spotify.py
@@ -10,6 +10,7 @@ DATABASE_LOCATION = "sqlite://my_played_tracks.sqlite"
 USER_ID = "sparat1k"
 TOKEN = "BQBc82wpH_6ekljb0R0glWQJ46d2PYDQDs6kntzlDf0k2EjO2ZCwgMjfOFf1ihpNL2eqY80mzlkt2yYzqgdzzYhCMNoCA2L0MZKWWZirqfhuAUVwGZLze2SPqXKvX3_FKP2nnrGngBIdzw"
 
+## these are some comments that I want to make
 if __name__ == "__main__":
     headers = {
         "Accept" : "application/json",
